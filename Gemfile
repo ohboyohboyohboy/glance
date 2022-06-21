fd6668205bbd0a253f1b589ddce10658e251c3fb
Kyle Yetter <kyetter@ebackpack.com>
2022-06-21 13:17:37 -0400

    Update dependency name and bump version

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem "ohboyohboyohboy-monocle-print", "~> 1.1"
+gem "monocle-print", "~> 1.1"
--- lib/glance.rb
+++ lib/glance.rb
@@ -11,7 +11,7 @@ library_dir = File.expand_path( "..", __FILE__ )
 $:.unshift( library_dir ) unless $:.include?( library_dir )
 
 module Glance
-  VERSION = "1.2.2"
+  VERSION = "1.2.3"
   include MonoclePrint
 
   autoload :ColorSequence, "glance/color-sequence"
